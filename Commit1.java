a901023d2bb9a6beaec80da529a28a200684aaa4
saurabh.patil@acellere.com <saurabh.patil@acellere.com>
2021-03-22 17:32:25 +0530

    Commit :1

---
+++ Commit1.java
@@ -0,0 +1,10 @@
+public class C:\vagrant\prodsanityui\testgws\testgwspr\Commit1.java {
+
+	public static void main(String args[]) {
+		try {
+			System.out.println("Screenshot taken for test : ");
+		} catch (Exception e) {
+			System.out.println("Exception " + e.getMessage());
+		}
+	}
+}
