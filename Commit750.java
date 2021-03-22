7f100133e8baf88b26bc186e4dca518b7f6e4767
saurabh.patil@acellere.com <saurabh.patil@acellere.com>
2021-03-22 17:57:43 +0530

    Commit :750

---
+++ Commit750.java
@@ -0,0 +1,10 @@
+public class Commit750 {
+
+	public static void main(String args[]) {
+		try {
+			System.out.println("Screenshot taken for test : ");
+		} catch (Exception e) {
+			System.out.println("Exception " + e.getMessage());
+		}
+	}
+}
