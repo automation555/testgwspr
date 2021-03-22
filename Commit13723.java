38a9f247d8d71da1d60b7b64dda2a822450d43cf
saurabh.patil@acellere.com <saurabh.patil@acellere.com>
2021-03-22 22:57:32 +0530

    Commit :13723

---
+++ Commit13723.java
@@ -0,0 +1,10 @@
+public class Commit13723 {
+
+	public static void main(String args[]) {
+		try {
+			System.out.println("Screenshot taken for test : ");
+		} catch (Exception e) {
+			System.out.println("Exception " + e.getMessage());
+		}
+	}
+}
