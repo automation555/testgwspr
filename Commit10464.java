3ca857eb267d3c0b5a1d76c79156f8f11d9053e0
saurabh.patil@acellere.com <saurabh.patil@acellere.com>
2021-03-22 23:18:37 +0530

    Commit :10464

---
+++ Commit10464.java
@@ -0,0 +1,10 @@
+public class Commit10464 {
+
+	public static void main(String args[]) {
+		try {
+			System.out.println("Screenshot taken for test : ");
+		} catch (Exception e) {
+			System.out.println("Exception " + e.getMessage());
+		}
+	}
+}
