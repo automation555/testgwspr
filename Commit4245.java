404b33cd72cba61153bbd0dae192093b167c4713
saurabh.patil@acellere.com <saurabh.patil@acellere.com>
2021-03-22 19:01:02 +0530

    Commit :4245

---
+++ Commit4245.java
@@ -0,0 +1,10 @@
+public class Commit4245 {
+
+	public static void main(String args[]) {
+		try {
+			System.out.println("Screenshot taken for test : ");
+		} catch (Exception e) {
+			System.out.println("Exception " + e.getMessage());
+		}
+	}
+}
