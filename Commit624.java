5b726abfbc45a7e0dbdc72d7aea4a93af1bafcc8
saurabh.patil@acellere.com <saurabh.patil@acellere.com>
2021-03-22 17:56:15 +0530

    Commit :624

---
+++ Commit624.java
@@ -0,0 +1,10 @@
+public class Commit624 {
+
+	public static void main(String args[]) {
+		try {
+			System.out.println("Screenshot taken for test : ");
+		} catch (Exception e) {
+			System.out.println("Exception " + e.getMessage());
+		}
+	}
+}
