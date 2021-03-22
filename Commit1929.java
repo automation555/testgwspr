f644b1a53f32e5574c1bf011398a455f5336fd60
saurabh.patil@acellere.com <saurabh.patil@acellere.com>
2021-03-22 18:12:56 +0530

    Commit :1929

---
+++ Commit1929.java
@@ -0,0 +1,10 @@
+public class Commit1929 {
+
+	public static void main(String args[]) {
+		try {
+			System.out.println("Screenshot taken for test : ");
+		} catch (Exception e) {
+			System.out.println("Exception " + e.getMessage());
+		}
+	}
+}
