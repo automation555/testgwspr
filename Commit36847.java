eed64f8c0013b683a3693087a44d022ed347377a
automation <automation@mygamma.io>
2021-04-01 20:36:34 +0530

    Commit :36847

---
+++ Commit36847.java
@@ -0,0 +1,10 @@
+public class Commit36847 {
+
+	public static void main(String args[]) {
+		try {
+			System.out.println("Screenshot taken for test : ");
+		} catch (Exception e) {
+			System.out.println("Exception " + e.getMessage());
+		}
+	}
+}
