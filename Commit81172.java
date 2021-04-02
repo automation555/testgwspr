62696ef542c1764cb1bc930d62f546fd772c208b
automation <automation@mygamma.io>
2021-04-02 11:49:44 +0530

    Commit :81172

---
+++ Commit81172.java
@@ -0,0 +1,10 @@
+public class Commit81172 {
+
+	public static void main(String args[]) {
+		try {
+			System.out.println("Screenshot taken for test : ");
+		} catch (Exception e) {
+			System.out.println("Exception " + e.getMessage());
+		}
+	}
+}
