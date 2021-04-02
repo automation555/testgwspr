b38cdabb0a7215b1b6ab1721e49b59d089f4f442
automation <automation@mygamma.io>
2021-04-02 09:53:28 +0530

    Commit :74134

---
+++ Commit74134.java
@@ -0,0 +1,10 @@
+public class Commit74134 {
+
+	public static void main(String args[]) {
+		try {
+			System.out.println("Screenshot taken for test : ");
+		} catch (Exception e) {
+			System.out.println("Exception " + e.getMessage());
+		}
+	}
+}
