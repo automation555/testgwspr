c06579b58d0aed041986fdbc1bc462e904d71ba3
automation <automation@mygamma.io>
2021-04-02 11:47:42 +0530

    Commit :63089

---
+++ Commit63089.java
@@ -0,0 +1,10 @@
+public class Commit63089 {
+
+	public static void main(String args[]) {
+		try {
+			System.out.println("Screenshot taken for test : ");
+		} catch (Exception e) {
+			System.out.println("Exception " + e.getMessage());
+		}
+	}
+}
