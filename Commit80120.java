c191f4fb024d93e82d94c5dd0907475971531f29
automation <automation@mygamma.io>
2021-04-02 09:44:05 +0530

    Commit :80120

---
+++ Commit80120.java
@@ -0,0 +1,10 @@
+public class Commit80120 {
+
+	public static void main(String args[]) {
+		try {
+			System.out.println("Screenshot taken for test : ");
+		} catch (Exception e) {
+			System.out.println("Exception " + e.getMessage());
+		}
+	}
+}
