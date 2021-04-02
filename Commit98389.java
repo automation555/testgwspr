97ab42303cca032783d9722cb777d159dd0444c3
automation <automation@mygamma.io>
2021-04-02 10:29:01 +0530

    Commit :98389

---
+++ Commit98389.java
@@ -0,0 +1,10 @@
+public class Commit98389 {
+
+	public static void main(String args[]) {
+		try {
+			System.out.println("Screenshot taken for test : ");
+		} catch (Exception e) {
+			System.out.println("Exception " + e.getMessage());
+		}
+	}
+}
